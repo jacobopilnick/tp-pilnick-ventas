b58217d1f541ce7c72f255b0c56b594997460f33
jacobopilnick <119467425+jacobopilnick@users.noreply.github.com>
2026-07-01 00:48:59 -0300

    Actualizacion completa del archivo
    Ahora si es un .py que toma los datos del csv, y se trabaja en base a eso, en la correccion comprendí que debia haber un grafico y para eso se usó el matplotlib que es algo nuevo que tuve que agregar, fue con la idea que haga grafico, y desconocia de la misma, esta muy buena.

--- scripts/analisis.py
+++ scripts/analisis.py
@@ -1,1 +1,35 @@
-print("Analisis de ventas")
+import csv
+import matplotlib.pyplot as plt
+
+fechas = []
+montos = []
+
+total = 0
+cantidad = 0
+
+with open("datos/ventas.csv", newline="", encoding="utf-8") as archivo:
+    lector = csv.DictReader(archivo)
+
+    for fila in lector:
+        fechas.append(fila["fecha_venta"])
+        monto = float(fila["monto_vendido"])
+        montos.append(monto)
+
+        total += monto
+        cantidad += 1
+
+promedio = total / cantidad
+
+print("Cantidad de ventas:", cantidad)
+print("Total vendido: $", total)
+print("Promedio de ventas: $", round(promedio, 2))
+
+plt.figure(figsize=(10,5))
+plt.bar(fechas, montos)
+plt.title("Ventas por día")
+plt.xlabel("Fecha")
+plt.ylabel("Monto vendido ($)")
+plt.xticks(rotation=45)
+
+plt.tight_layout()
+plt.show()
